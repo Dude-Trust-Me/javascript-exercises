ca2ba78a295b53ec9449c90abf27a41950061a33
Dude-Trust-Me <iankent555@gmail.com>
2023-06-04 20:17:44 +0800

    June 4 2023

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    if (times < 0) "ERROR";
+    let string = "hey"
+    for (let i = 0; i < times; i++){
+        string += word;
+    }return string;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(x,y) {
+    let sum = 0;
+    if (x<0 || y < 0 || isNaN(x) || isNaN(y) || Number(x) !== x || Number(y) !== y) return "ERROR";
+    
+    else if(x<y){
+        for (let i = x; i<=y;i++){
+            sum +=i;
+            
+        }return sum;
+    }else {
+        for (let i = y; i<=x; i++){
+            sum+=i;
+            
+        }return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
